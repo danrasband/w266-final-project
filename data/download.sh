8ec1e1d6d58705646b01e8c486b2c1a1dbc803e6
Dan Rasband <danrasband@gmail.com>
2018-11-14 09:58:25 -0700

    Add download script for getting glozman books

---
+++ data/download.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+wget "http://www.glozman.com/TextPages/Clancy Tom - Patriot Games.txt" -O clancy_tom_patriot_games.txt
+wget "http://www.glozman.com/TextPages/Clancy Tom - Red Storm Rising.txt" -O clancy_tom_red_storm_rising.txt
+wget "http://www.glozman.com/TextPages/Clarke Arthur C - 3001 The Final Odissey.txt" -O clarke_arthur_c_3001_the_final_odissey.txt
+wget "http://www.glozman.com/TextPages/Frank Herbert - Children of Dune.txt" -O frank_herbert_children_of_dune.txt
+wget "http://www.glozman.com/TextPages/Frank Herbert - Dune Messiah.txt" -O frank_herbert_dune_messiah.txt
+wget "http://www.glozman.com/TextPages/Frank Herbert - Dune.txt" -O frank_herbert_dune.txt
+wget "http://www.glozman.com/TextPages/Harry Potter 1 - Sorcerer's Stone.txt" -O harry_potter_1_sorcerer_s_stone.txt
+wget "http://www.glozman.com/TextPages/Harry Potter 2 - Chamber of Secrets.txt" -O harry_potter_2_chamber_of_secrets.txt
+wget "http://www.glozman.com/TextPages/Harry Potter 3 - The Prisoner Of Azkaban.txt" -O harry_potter_3_the_prisoner_of_azkaban.txt
+wget "http://www.glozman.com/TextPages/Harry Potter 4 - The Goblet Of Fire.txt" -O harry_potter_4_the_goblet_of_fire.txt
+wget "http://www.glozman.com/TextPages/Harry Potter 5 - Order of the Phoenix.txt" -O harry_potter_5_order_of_the_phoenix.txt
+wget "http://www.glozman.com/TextPages/Harry Potter 6 - The Half Blood Prince.txt" -O harry_potter_6_the_half_blood_prince.txt
+wget "http://www.glozman.com/TextPages/Harry Potter 7 - Deathly Hollows.txt" -O harry_potter_7_deathly_hollows.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Strike at Shayol Ghul.txt" -O robert_jordan_the_strike_at_shayol_ghul.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 01 - Eye of the world.txt" -O robert_jordan_the_wheel_of_time_01_eye_of_the_world.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 02 - The Great Hunt.txt" -O robert_jordan_the_wheel_of_time_02_the_great_hunt.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 03 - The Dragon Reborn.txt" -O robert_jordan_the_wheel_of_time_03_the_dragon_reborn.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 04 - The Shadow Rising.txt" -O robert_jordan_the_wheel_of_time_04_the_shadow_rising.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 05 - The Fires of Heaven.txt" -O robert_jordan_the_wheel_of_time_05_the_fires_of_heaven.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 06 - Lord of Chaos.txt" -O robert_jordan_the_wheel_of_time_06_lord_of_chaos.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 07 - A Crown of Swords.txt" -O robert_jordan_the_wheel_of_time_07_a_crown_of_swords.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 08 - The Path of Daggers.txt" -O robert_jordan_the_wheel_of_time_08_the_path_of_daggers.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 09 - Winters Heart.txt" -O robert_jordan_the_wheel_of_time_09_winters_heart.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 10 - Crossroads of Twilight.txt" -O robert_jordan_the_wheel_of_time_10_crossroads_of_twilight.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time 11 - Knife of Dreams.txt" -O robert_jordan_the_wheel_of_time_11_knife_of_dreams.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time Prelude - New Spring.txt" -O robert_jordan_the_wheel_of_time_prelude_new_spring.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - The Wheel of Time Compendium.txt" -O robert_jordan_the_wheel_of_time_compendium.txt
+wget "http://www.glozman.com/TextPages/Robert Jordan - 12 - The Gathering Storm - Chapter One.txt" -O robert_jordan_12_the_gathering_storm_chapter_one.txt
+wget "http://www.glozman.com/TextPages/R.A. Salvatore - The Dark Elf Trilogy.txt" -O r_a_salvatore_the_dark_elf_trilogy.txt
+wget "http://www.glozman.com/TextPages/R.A.Salvatore - Icewind Dale Trilogy 1 - The Crystal Shard.txt" -O r_a_salvatore_icewind_dale_trilogy_1_the_crystal_shard.txt
+wget "http://www.glozman.com/TextPages/R.A. Salvatore - The Icewind Dale Trilogy - 2 - Streams of Silver.txt" -O r_a_salvatore_the_icewind_dale_trilogy_2_streams_of_silver.txt
+wget "http://www.glozman.com/TextPages/R.A. Salvatore - The Icewind Dale Trilogy - 3 - The Halfling's Gem.txt" -O r_a_salvatore_the_icewind_dale_trilogy_3_the_halfling_s_gem.txt
+wget "http://www.glozman.com/TextPages/R.A. Salvatore - The Legacy of the Drow 1 - Legacy.txt" -O r_a_salvatore_the_legacy_of_the_drow_1_legacy.txt
+wget "http://www.glozman.com/TextPages/RA SalvatoreThe Legacy of The Drow - 2 - Starless Night.txt" -O r_a_salvatore_the_legacy_of_the_drow_2_starless_night.txt
+wget "http://www.glozman.com/TextPages/R.A. Salvatore - The Legacy of the Drow III - Siege of Darkness.txt" -O r_a_salvatore_the_legacy_of_the_drow_i_i_i_siege_of_darkness.txt
+wget "http://www.glozman.com/TextPages/R.A. Salvatore - The Legacy of the Drow IV - Passage to Dawn.txt" -O r_a_salvatore_the_legacy_of_the_drow_i_v_passage_to_dawn.txt
+wget "http://www.glozman.com/TextPages/Salvatore, R.A. - Paths of Darkness 1 - The Silent Blade.txt" -O salvatore_r_a_paths_of_darkness_1_the_silent_blade.txt
+wget "http://www.glozman.com/TextPages/Salvatore, R.A. - Paths of Darkness 2 - The Spine of the World.txt" -O salvatore_r_a_paths_of_darkness_2_the_spine_of_the_world.txt
+wget "http://www.glozman.com/TextPages/Salvatore, R.A. - Paths of Darkness 3 - Servant of the Shard.txt" -O salvatore_r_a_paths_of_darkness_3_servant_of_the_shard.txt
+wget "http://www.glozman.com/TextPages/Salvatore, RA - Cleric Quintet 1 - Canticle.txt" -O salvatore_r_a_cleric_quintet_1_canticle.txt
+wget "http://www.glozman.com/TextPages/Salvatore, RA - Cleric Quintet 2 - In Sylvan Shadows.txt" -O salvatore_r_a_cleric_quintet_2_in_sylvan_shadows.txt
+wget "http://www.glozman.com/TextPages/Salvatore, RA - Cleric Quintet 3 - Night Masks.txt" -O salvatore_r_a_cleric_quintet_3_night_masks.txt
+wget "http://www.glozman.com/TextPages/Salvatore, RA - Cleric Quintet 4 - The Fallen Fortress.txt" -O salvatore_r_a_cleric_quintet_4_the_fallen_fortress.txt
+wget "http://www.glozman.com/TextPages/Salvatore, RA - Cleric Quintet 5 -The Chaos Curse.txt" -O salvatore_r_a_cleric_quintet_5_the_chaos_curse.txt
+wget "http://www.glozman.com/TextPages/01 The Thousand Orcs.txt" -O 01_the_thousand_orcs.txt
+wget "http://www.glozman.com/TextPages/02 The Lone Drow.txt" -O 02_the_lone_drow.txt
+wget "http://www.glozman.com/TextPages/03 The Two Swords.txt" -O 03_the_two_swords.txt
+wget "http://www.glozman.com/TextPages/01 Spellfire.txt" -O 01_spellfire.txt
+wget "http://www.glozman.com/TextPages/02 Crown of Fire.txt" -O 02_crown_of_fire.txt
+wget "http://www.glozman.com/TextPages/01 Horselords.txt" -O 01_horselords.txt
+wget "http://www.glozman.com/TextPages/02 Dragonwall.txt" -O 02_dragonwall.txt
+wget "http://www.glozman.com/TextPages/03 Crusade.txt" -O 03_crusade.txt
+wget "http://www.glozman.com/TextPages/01 Shadowdale.txt" -O 01_shadowdale.txt
+wget "http://www.glozman.com/TextPages/02 Tantras.txt" -O 02_tantras.txt
+wget "http://www.glozman.com/TextPages/03 Waterdeep.txt" -O 03_waterdeep.txt
+wget "http://www.glozman.com/TextPages/04 Prince of Lies.txt" -O 04_prince_of_lies.txt
+wget "http://www.glozman.com/TextPages/05 Crucible - The Trial Of Cyric The Mad.txt" -O 05_crucible_the_trial_of_cyric_the_mad.txt
+wget "http://www.glozman.com/TextPages/01 Dragons of Autumn Twilight.txt" -O 01_dragons_of_autumn_twilight.txt
+wget "http://www.glozman.com/TextPages/02 Dragons of Winter Night.txt" -O 02_dragons_of_winter_night.txt
+wget "http://www.glozman.com/TextPages/03 Dragons of Spring Dawning.txt" -O 03_dragons_of_spring_dawning.txt
+wget "http://www.glozman.com/TextPages/01 Time Of The Twins.txt" -O 01_time_of_the_twins.txt
+wget "http://www.glozman.com/TextPages/02 War Of The Twins.txt" -O 02_war_of_the_twins.txt
+wget "http://www.glozman.com/TextPages/03 Test Of The Twins.txt" -O 03_test_of_the_twins.txt
+wget "http://www.glozman.com/TextPages/01 The Second Generation.txt" -O 01_the_second_generation.txt
+wget "http://www.glozman.com/TextPages/02 Dragons of Summer Flame.txt" -O 02_dragons_of_summer_flame.txt
+wget "http://www.glozman.com/TextPages/Dragonlance Preludes 2 vol 2 - Flint the King.txt" -O dragonlance_preludes_2_vol_2_flint_the_king.txt
+wget "http://www.glozman.com/TextPages/01 The Legend Of Huma.txt" -O 01_the_legend_of_huma.txt
+wget "http://www.glozman.com/TextPages/Lloyd Alexander - [Chronicles Of Prydain 1] Book of Three.txt" -O lloyd_alexander_chronicles_of_prydain_1_book_of_three.txt
+wget "http://www.glozman.com/TextPages/Lloyd Alexander - [Chronicles Of Prydain 2] Black Cauldron.txt" -O lloyd_alexander_chronicles_of_prydain_2_black_cauldron.txt
+wget "http://www.glozman.com/TextPages/Lloyd Alexander - [Chronicles Of Prydain 3] Castle Of Llyr.txt" -O lloyd_alexander_chronicles_of_prydain_3_castle_of_llyr.txt
+wget "http://www.glozman.com/TextPages/Lloyd Alexander - [Chronicles Of Prydain 4] Taran Wanderer.txt" -O lloyd_alexander_chronicles_of_prydain_4_taran_wanderer.txt
+wget "http://www.glozman.com/TextPages/Lloyd Alexander - [Chronicles Of Prydain 5] High King.txt" -O lloyd_alexander_chronicles_of_prydain_5_high_king.txt
+wget "http://www.glozman.com/TextPages/Auel, Jean - Earth's Children 01 - The Clan of the Cave Bear.txt" -O auel_jean_earth_s_children_01_the_clan_of_the_cave_bear.txt
+wget "http://www.glozman.com/TextPages/Auel, Jean - Earth's Children 02 - The Valley of Horses.txt" -O auel_jean_earth_s_children_02_the_valley_of_horses.txt
+wget "http://www.glozman.com/TextPages/Auel, Jean - Earth's Children 03 - The Mammoth Hunters.txt" -O auel_jean_earth_s_children_03_the_mammoth_hunters.txt
+wget "http://www.glozman.com/TextPages/Auel, Jean - Earth's Children 04 - Plains Of Passage.txt" -O auel_jean_earth_s_children_04_plains_of_passage.txt
+wget "http://www.glozman.com/TextPages/Auel, Jean - Earth's Children 05 - The Shelters Of Stone.txt" -O auel_jean_earth_s_children_05_the_shelters_of_stone.txt
+wget "http://www.glozman.com/TextPages/Star Wars - [Thrawn Trilogy 01] - Heir to the Empire (by Timothy Zahn).txt" -O star_wars_thrawn_trilogy_01_heir_to_the_empire_by_timothy_zahn.txt
+wget "http://www.glozman.com/TextPages/Star Wars - [Thrawn Trilogy 02] - Dark Force Rising (by Timothy Zahn).txt" -O star_wars_thrawn_trilogy_02_dark_force_rising_by_timothy_zahn.txt
+wget "http://www.glozman.com/TextPages/Star Wars - [Thrawn Trilogy 03] - The Last Command (by Timothy Zahn).txt" -O star_wars_thrawn_trilogy_03_the_last_command_by_timothy_zahn.txt
+wget "http://www.glozman.com/TextPages/01 - The Fellowship Of The Ring.txt" -O 01_the_fellowship_of_the_ring.txt
+wget "http://www.glozman.com/TextPages/02 - The Two Towers.txt" -O 02_the_two_towers.txt
+wget "http://www.glozman.com/TextPages/03 - The Return Of The King.txt" -O 03_the_return_of_the_king.txt
+wget "http://www.glozman.com/TextPages/04 - Appendices And Index.txt" -O 04_appendices_and_index.txt
