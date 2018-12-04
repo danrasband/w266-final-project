10b3d5dbcd3c6a8255f4a91902acb35fa1aebdc3
Dan Rasband <danrasband@gmail.com>
2018-12-04 00:06:45 -0700

    Add make_entity_id function to data helpers
    This function can be used to convert an entity's identifying data
    into a single string.

--- ontonotes_graphing/helpers/data.py
+++ ontonotes_graphing/helpers/data.py
@@ -18,3 +18,12 @@ def train_dev_test_split_ids(ids, train_portion=0.7, dev_portion=0.2, seed=None)
     dev_end = n_train + int(n * dev_portion)
     
     return [ids[0:n_train], ids[n_train:dev_end], ids[dev_end:]]
+
+
+def make_entity_id(document_id, sentence_index, start_word_index, end_word_index):
+    return ':'.join([
+        document_id,
+        str(sentence_index),
+        str(start_word_index),
+        str(end_word_index),
+    ])
